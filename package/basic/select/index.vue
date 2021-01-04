c29d2c14a9c36471b9b1c7013b9360f94bdcddf9
junkai <576327324@qq.com>
2021-01-04 16:45:04 +0800

    fix: 修复selector中动态加载数据的处理函数回调不执行问题

--- package/basic/select/index.vue
+++ package/basic/select/index.vue
@@ -37,19 +37,16 @@ export default {
         this.curlCommand(this.element.config.dynamicUrl, params => {
           params.data.url += "/" + query;
           if (params.type === "request" || params.type === "http") {
-            executeProtocol
-              .call(this, params)
-              .then(res => {
+            executeProtocol.call(this, {
+              ...params,
+              cb: res => {
                 if (res && res.code === 200) {
                   this.options = res.data;
                 } else {
                   this.options = [];
                 }
-              })
-              .catch(err => {
-                console.log(err);
-                this.options = [];
-              });
+              }
+            });
           }
         });
       } else {
@@ -60,7 +57,20 @@ export default {
   mounted() {
     let dynamic = this.element.config.dynamic;
     if (dynamic) {
-      this.options = [];
+      this.curlCommand(this.element.config.dynamicUrl, params => {
+        if (params.type === "request" || params.type === "http") {
+          executeProtocol.call(this, {
+            ...params,
+            cb: res => {
+              if (res && res.code === 200) {
+                this.options = res.data;
+              } else {
+                this.options = [];
+              }
+            }
+          });
+        }
+      });
     } else {
       this.options = this.element.config.options;
     }
